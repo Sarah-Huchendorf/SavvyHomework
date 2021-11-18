e4d1759a4dd07e1b46e203e6b45960cddd0fff05
Sarah Huchendorf <sarah.coding@outlook.com>
2021-11-17 16:39:46 -0800

    Creation of Week 2 Homework

---
+++ Week2Homework/week2.js
@@ -0,0 +1,19 @@
+//Creation of variable pizzaPlace and assigned it a value
+const pizzaPlace = `Sarah's Pizza`;
+//creation of variable numberOfToppings and assigned it a value
+let numberOfToppings = 8;
+//printing the variable pizzaPlace to the console
+console.log(pizzaPlace);
+//printing the variable numberOfToppings to the console
+console.log(numberOfToppings);
+//printing a sentence to the console using both variables pizzaPlace and numberOfToppings.
+console.log(`"At ${pizzaPlace},
+we have ${numberOfToppings} toppings."`)
+//if the variable numberOfToppings is less than 10 than it will print Quality not Quantity. If the variable numberOfToppings is more than 10 than it will print A whole lot of pizza
+ if (numberOfToppings <10) {
+console.log("Quality, not Quantity.")
+} else {
+console.log("A whole lot of pizza")
+}
+
+
